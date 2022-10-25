d4eb0f2c720456b1d055e213184275b99d4ebe21
TengChen <teng.chen11@gmail.com>
2022-10-25 11:11:28 +0800

    MNIST优化

--- eznf/datasets/MNIST.py
+++ eznf/datasets/MNIST.py
@@ -2,10 +2,10 @@ import requests
 import time
 import os
 import numpy as np
-import struct
 import gzip
 import random
 import matplotlib.pyplot as plt
+from io import BytesIO
 
 class MNIST:
     url = 'http://yann.lecun.com/exdb/mnist/'
@@ -15,120 +15,62 @@ class MNIST:
         self.root = root 
         self.download = download
         self.shuffle = shuffle
+        
+    def __unpack(self):
+        '''Return
+            data->list
+        '''
+        data = []
+        for idx, val in enumerate(MNIST.resources):
+            file_gz = MNIST.url + val + '.gz'
+            time.sleep(0.5)
+            response = requests.get(file_gz)
+            
+            if self.download == True:
+                if not os.path.exists(self.root + '\\' + val + '.gz'):
+                    with open(self.root + '\\' + val + '.gz', 'wb') as file:
+                        file.write(response.content)
+                    
+            buffer = BytesIO(response.content)
+            g_file = gzip.GzipFile(fileobj = buffer)
+            
+            if idx == 0 or idx == 2:
+                sub_data = np.frombuffer(g_file.read(), dtype = np.uint8)[16:].reshape(-1, 28, 28)
+                data.append(sub_data)
+                     
+            elif idx == 1 or idx == 3:
+                sub_data = np.frombuffer(g_file.read(), dtype = np.uint8)[8:]
+                data.append(sub_data)
+
+        return data
     
     def get(self):
-        """Returns:
-            X_train -> Tensor: train data
-            Y_train -> Tensor: train label
-            X_test -> Tensor: test data
-            Y_test -> Tensor: test label
-        """
-        if self.download == True:
-            data = []
+        '''Returns:
+           X_train -> Tensor: train_data 
+           Y_train -> Tensor: train_label
+           X_test -> Tensor: test_data
+           Y_test -> Tensor: test_label
+        '''
+        data = self.__unpack()
+        self.X_train, self.Y_train, self.X_test, self.Y_test = data[0], data[1], data[2], data[3]
             
-            for idx, val in enumerate(MNIST.resources):    
-                file_gz = MNIST.url + val + '.gz'
-                time.sleep(0.5)
-                response = requests.get(file_gz)
+        if self.shuffle == True:
+            self.X_shuffle_train = []
+            self.Y_shuffle_train = []
+            idxs = np.random.permutation(self.X_train.shape[0])
+            for idx,val in enumerate(idxs):
+                self.X_shuffle_train.append(self.X_train[val])
+                self.Y_shuffle_train.append(self.Y_train[val])
                 
-                filename = self.root + '\\' + val + '.gz'
+            return np.array(self.X_shuffle_train), np.array(self.Y_shuffle_train), self.X_test, self.Y_test
                 
-                with open(filename, 'wb') as file:
-                    file.write(response.content)
-                
-                g_file = gzip.GzipFile(filename)
-                f_name = filename.replace(".gz", "")
-                open(f_name, 'wb').write(g_file.read())
-                g_file.close()
-                
-                with open(f_name, 'rb') as file:
-                    if idx == 0 or idx == 2:
-                        struct.unpack('>4i', file.read(16))
-                        sub_data = np.fromfile(file, dtype = np.uint8).reshape(-1, 784)
-                        data.append(sub_data)
-                     
-                    elif idx == 1 or idx == 3:
-                        struct.unpack('>2i', file.read(8))
-                        sub_data = np.fromfile(file, dtype = np.uint8)
-                        data.append(sub_data)
-                        
-                os.remove(f_name)
-            
-            
-            self.X_train, self.Y_train, self.X_test, self.Y_test = data[0], data[1], data[2], data[3]
-            
-            
-            if self.shuffle == True:
-                X_shuffle_train = []
-                Y_shuffle_train = []
-                idxs = np.random.permutation(X_train.shape[0])
-                for idx,val in enumerate(idxs):
-                    X_shuffle_train[idx] = X_train[val]
-                    Y_shuffle_train[idx] = Y_train[val]
-                    
-                return X_shuffle_train, Y_shuffle_train, X_test, Y_test
-                
-            else:
-                return self.X_train, self.Y_train, self.X_test, self.Y_test
-           
-            
-        
         else:
-            data = []
-
-            for idx, val in enumerate(MNIST.resources):
-                file_gz = MNIST.url + val + '.gz'
-                time.sleep(0.5)
-                response = requests.get(file_gz)
-                
-                filename = self.root + '\\' + val + '.gz'
-                with open(filename, 'wb') as file:
-                    file.write(response.content)
-                
-                g_file = gzip.GzipFile(filename)
-                
-                f_name = filename.replace(".gz", "")
-                open(f_name, 'wb').write(g_file.read())
-                g_file.close()
-                
-                with open(f_name, 'rb') as file:
-                    if idx == 0 or idx == 2:
-                        struct.unpack('>4i', file.read(16))
-                        sub_data = np.fromfile(file, dtype = np.uint8).reshape(-1, 784)
-                        data.append(sub_data)
-                     
-                    elif idx == 1 or idx == 3:
-                        struct.unpack('>2i', file.read(8))
-                        sub_data = np.fromfile(file, dtype = np.uint8)
-                        data.append(sub_data)
-                os.remove(f_name)
-                os.remove(filename)
-            
-            X_train, Y_train, X_test, Y_test = data[0], data[1], data[2], data[3]
-            
-            if self.shuffle == True:
-                X_shuffle_train = []
-                Y_shuffle_train = []
-                idxs = np.random.permutation(X_train.shape[0])
-                
-                for idx,val in enumerate(idxs):
-                    X_shuffle_train[idx] = X_train[val]
-                    Y_shuffle_train[idx] = Y_train[val]
-                    
-                return X_shuffle_train, Y_shuffle_train, X_test, Y_test
-                
-                
-            else:
-                return X_train, Y_train, X_test, Y_test
-            
+            return self.X_train, self.Y_train, self.X_test, self.Y_test
+        
     def show_one_sample(self):
-        idx = random.randint(0,60000)
-        plt.imshow(self.X_train[idx].reshape(-1, 28), cmap = 'gray')
-        print('label:{}'.format(self.Y_train[idx]))
-
-    def unpack(self, args):
-        pass
-
-#root = r'C:\Users\Lenovo\Desktop\python\dataset'
-#test = MNIST(root = root, shuffle = False, download = False)
-#X_train, Y_train, X_test, Y_test = test.get()
+        '''Return
+            Null
+        '''
+        idx = random.randint(0, 10000)
+        plt.imshow(self.X_test[idx].reshape(-1, 28), cmap = 'gray')
+        print('label:{}'.format(self.Y_test[idx]))
