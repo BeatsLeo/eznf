6b9935c0c26b22a90c3f6a1d7f3ba17585824508
E022-23093 <2275794346@qq.com>
2022-11-22 13:20:38 +0800

    Update functional.py

--- eznf/nn/functional.py
+++ eznf/nn/functional.py
@@ -1,13 +1,49 @@
-import eznf
+# import eznf
+from cmath import tan
+import torch
+import numpy as np
 
-def relu() -> eznf.Tensor:
-    pass
 
-def sigmoid() -> eznf.Tensor:
-    pass
 
-def drelu() -> eznf.Tensor:
-    pass
 
-def linear() -> eznf.Tensor:
-    pass
+#支持gpu暂且没写，而且只用了pytorch里面的tensor
+a=torch.tensor([[1,1],[1,1],[1,1]])
+b=np.array([1,1])
+
+def istensor(input):
+    if(isinstance(input,torch.Tensor)!=True):
+        raise ValueError('not tensor')
+    
+
+def linear(input, weight, bias=None)-> torch.Tensor: 
+    istensor(input)
+    return input @ weight.T + bias
+
+
+def relu(input) -> torch.Tensor:
+    istensor(input)
+    shape = input.shape
+    input=torch.reshape(input,(-1,1))
+    for i in range(len(input)):
+        input[i]=max(0,input[i])
+    output=torch.reshape(input,shape)
+    return output
+        
+def sigmoid(input) -> torch.Tensor:
+    istensor(input)
+    return 1.0/(1.0+torch.exp(-input))
+
+def tanh(input) -> torch.Tensor:
+    istensor(input)
+    return (torch.exp(input)-torch.exp(-input))/(torch.exp(input)+torch.exp(-input))
+
+def softmax(input, dim=None)-> torch.Tensor:        #stacklevel=3, dtype=None 没管,dim写不来
+    istensor(input)                        
+    exp_input=torch.tensor(input)
+    exp_sum = torch.sum(exp_input)
+    return exp_input/exp_sum
+
+
+# def conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) -> torch.Tensor: 
+    
+    
