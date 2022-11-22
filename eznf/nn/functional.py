a037cb47c12a12be3c79b8f8f0f8617576af78c2
E022-23093 <2275794346@qq.com>
2022-11-22 15:20:36 +0800

    卷积好难

--- eznf/nn/functional.py
+++ eznf/nn/functional.py
@@ -4,8 +4,10 @@ import numpy as np
 
 
 #支持gpu暂且没写，而且只用了pytorch里面的tensor
-a=torch.tensor([[1,1],[1,1],[1,1]])
-b=np.array([1,1])
+a=torch.tensor([[[[1,1],[1,1]],[[2,2],[2,2]]],[[[1,1],[1,1]],[[2,2],[2,2]]]])
+b=np.array([[[[1,1,1],[1,1,1]],[[2,2,2],[2,2,2]]],[[[1,1,1],[1,1,1]],[[2,2,2],[2,2,2]]]])
+b=np.transpose(b,(2,3,1,0))
+print(b.shape)
 
 def istensor(input):
     if(isinstance(input,torch.Tensor)!=True):
@@ -39,11 +41,28 @@ def softmax(input)-> torch.Tensor:
     return exp_input/exp_sum
 
 
+# def conv1d(input,input_channel, output_channel,kernal_size, stride=1, padding=0) -> torch.Tensor: 
+# def conv2d(input,input_channel, output_channel,kernal_size, stride=1, padding=0) -> torch.Tensor: 
+# def conv3d(input,input_channel, output_channel,kernal_size, stride=1, padding=0) -> torch.Tensor: 
 
 
-# def conv1d(input, sad, bias=None, stride=1, padding=0) -> torch.Tensor: 
-# def conv2d(input, weight, bias=None, stride=1, padding=0) -> torch.Tensor: 
-# def conv3d(input, weight, bias=None, stride=1, padding=0) -> torch.Tensor: 
+
+def conv2d(x,input_channel,output_channel,kernal_size):       #默认无padding，移动1
+    #x  [width,height,channel,batch]                                               
+    kernal = np.random.randn(output_channel,kernal_size,kernal_size) 
+    width,height,batch=x.shape[0],x.shape[1],x.shape[3]           
+    
+    out=np.zeros([width-kernal_size+1,height-kernal_size+1,output_channel,batch])
+    for k in range(batch):
+        for i in range(output_channel):
+            for j in range(input_channel):
+                for a in range(width-kernal_size+1):
+                    for b in range(height-kernal_size+1):
+                        out[a,b,i,k]+=np.sum(np.sum(x[a:a+kernal_size,b:b+kernal_size,j,k]*kernal[i]))
+                    if b==width-kernal_size and a!=height-kernal_size:
+                        b=0
+    return out,kernal
+
 
 
 
