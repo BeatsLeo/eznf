bdfe4eb454c1e8a25c110c7eba2a5077419bc171
E022-23093 <2275794346@qq.com>
2022-11-27 22:17:07 +0800

    陈腾的

--- eznf/datasets/Cifar10.py
+++ eznf/datasets/Cifar10.py
@@ -1,3 +1,63 @@
-class Cifar10:
-    def __init__(self):
-        pass
+import requests
+import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+class cifar10:
+    url = 'http://www.cs.toronto.edu/~kriz'
+    resource = ['cifar-10-python.tar.gz']
+    resources = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
+
+    def __init__(self, root:str):
+        self.root = root
+
+    def __unpickle(self, path:str):
+        X_train = []
+        Y_train = []
+        X_test = []
+        Y_test = []
+
+        for _, val in enumerate(cifar10.resources):
+            if val != 'test_batch':
+                file = path + '\\' + val
+                with open(file, 'rb') as f:
+                    dict = pickle.load(f, encoding = 'bytes')
+        
+                    for _, val in enumerate(dict[b'data']):
+                        X_train.append(val)
+        
+                    for val in dict[b'labels']:
+                        Y_train.append(val)
+            else:
+                file = path + '\\' + val
+                with open(file, 'rb') as f:
+                    dict = pickle.load(f, encoding = 'bytes')
+        
+                    for _, val in enumerate(dict[b'data']):
+                        X_test.append(val)
+        
+                    for val in dict[b'labels']:
+                        Y_test.append(val)
+        return X_train, Y_train, X_test, Y_test
+
+    def download(self):
+        '''
+        Return:
+        path -> str
+        '''
+        file_gz = cifar10.url + '/' + cifar10.resource[0]
+        time.sleep(0.5)
+        response = requests.get(file_gz)
+
+        if not os.path.exists(self.root + '\\' + cifar10.resource[0]):
+            with open(self.root + '\\' + cifar10.resource[0], 'wb') as file:
+                file.write(response.content)
+
+
+        return self.root + '\\' + 'cifar-10-batches-py'
+            
+    def get(self, path:str):
+        X_train, Y_train, X_test, Y_test = self.__unpickle(path)
+        return np.array(X_train), np.array(Y_train), np.array(X_test), np.array(Y_test)
--- eznf/optim/SGD.py
+++ eznf/optim/SGD.py
@@ -1,8 +1,16 @@
+import eznf
 from eznf import Tensor
 
 class SGD:
-    def __init__(self, alpha, model):
-        pass
+    def __init__(self, alpha:float, model:eznf.nn.Module):
+        self.alpha = alpha
+        self.m = model
 
-    def step():
-        pass
+    def step(self):
+        for w in self.m.parameters():
+            w.item = w.item - self.alpha * w.grad.item
+            
+    def zero_grad(self):
+        for w in self.m.parameters():
+            w.grad.item = 0
+    
