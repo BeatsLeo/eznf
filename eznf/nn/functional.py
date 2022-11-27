63263db593af38c53c6dff47a3fd9049e47a4f40
E022-23093 <2275794346@qq.com>
2022-11-27 18:26:07 +0800

    删除了pytorch测试的代码

--- eznf/nn/functional.py
+++ eznf/nn/functional.py
@@ -1,16 +1,7 @@
-# import eznf
-import torch
-import numpy as np
-
-
-#支持gpu暂且没写，而且只用了pytorch里面的tensor
-a=torch.tensor([[[[1,1],[1,1]],[[2,2],[2,2]]],[[[1,1],[1,1]],[[2,2],[2,2]]]])
-b=np.array([[[[1,1,1],[1,1,1]],[[2,2,2],[2,2,2]]],[[[1,1,1],[1,1,1]],[[2,2,2],[2,2,2]]]])
-b=np.transpose(b,(2,3,1,0))
-print(b.shape)
+from eznf.tensor.tensor import Tensor
 
 def istensor(input):
-    if(isinstance(input,torch.Tensor)!=True):
+    if(isinstance(input,Tensor)!=True):
         raise ValueError('not tensor')
     
 
@@ -21,41 +12,23 @@ def linear(input, weight, bias=None):
 
 def relu(input) :
     istensor(input)
-    input=np.array(input)
-    output = np.maximum(0,input)
-    output=torch.tensor(output)
-    return output
-        
+    return input * (input > 0)
+
+
 def sigmoid(input) :
     istensor(input)
-    return 1.0/(1.0+torch.exp(-input))
+    return 1.0/(1.0+Tensor.exp(-input))
+
 
 def tanh(input) :
     istensor(input)
-    return (torch.exp(input)-torch.exp(-input))/(torch.exp(input)+torch.exp(-input))
+    return (Tensor.exp(input)-Tensor.exp(-input))/(Tensor.exp(input)+Tensor.exp(-input))
 
 def softmax(input):      
-    istensor(input)                        
-    exp_input=torch.tensor(input)
-    exp_sum = torch.sum(exp_input)
+    istensor(input)                      
+    exp_input=Tensor.exp(input)
+    exp_sum = sum(exp_input)
     return exp_input/exp_sum
-
-def conv2d(x,input_channel,output_channel,kernal_size):       #默认无padding，移动1
-    #x  [width,height,channel,batch]                                               
-    kernal = np.random.randn(output_channel,kernal_size,kernal_size) 
-    width,height,batch=x.shape[0],x.shape[1],x.shape[3]           
-    
-    out=np.zeros([width-kernal_size+1,height-kernal_size+1,output_channel,batch])
-    for k in range(batch):
-        for i in range(output_channel):
-            for j in range(input_channel):
-                for a in range(width-kernal_size+1):
-                    for b in range(height-kernal_size+1):
-                        out[a,b,i,k]+=np.sum(np.sum(x[a:a+kernal_size,b:b+kernal_size,j,k]*kernal[i]))
-                    if b==width-kernal_size and a!=height-kernal_size:
-                        b=0
-    return out,kernal
-
 
 def mes_loss(input,y):  
     istensor(input)
@@ -68,8 +41,7 @@ def mes_loss(input,y):
 def cross_entropy(input,y):  
     istensor(input)
     istensor(y)
-    input=softmax(input)
-    input=torch.log(input)
+    input=Tensor.log(input)
     output=0
     for i in range(len(input)):
         output += -y[i]*input[i]
